0f669358d8e904f3b5c07ad37a70bfea79082874
Sroop <sroopsunar@hotmail.com>
2014-05-27 17:52:55 +0100

    added routes for comments, a form. need to sort create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'comments/new'
+
   devise_for :users
   root 'posts#index'
 
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
