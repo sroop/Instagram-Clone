fe9fa43e7a9a6cba51b9b9020eb268c73e3363d3
Sroop <sroopsunar@hotmail.com>
2014-05-29 11:19:03 +0100

    outlined three feature tests for tagging

---
+++ spec/features/tagging_feature_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe 'Tags' do
+
+	context 'Displaying tags on the home page' do
+
+		it 'For a post with zero tags, no tags are displayed' do
+
+		end
+
+		it 'For a post with one tag, one tag is displayed' do
+
+		end
+
+		it 'For a post with two tags, two tags are displayed' do
+
+		end
+
+		#unit testing in spec/models/post_spec.rb for a more detailed breakdown of the tags behaviour
+		
+	end
+
+end
