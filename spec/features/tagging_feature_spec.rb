b7da03c9ddec92ea96f4d01b1eb2e8002150da9e
Sroop <sroopsunar@hotmail.com>
2014-05-29 17:43:57 +0100

    created new test for filtering posts by tags, added routes resources and added the correct path into tag name link

--- spec/features/tagging_feature_spec.rb
+++ spec/features/tagging_feature_spec.rb
@@ -28,4 +28,30 @@ describe 'Tags' do
 
 	end
 
+	context 'Filtering posts by tag' do
+
+		before do
+			Post.create(caption: "Another random post")
+			user = User.create(email: 'sroop@sunar.com', password: '123456789', password_confirmation: '123456789')
+			login_as user
+		end
+
+		it 'Posts are displayed by chosen tag' do
+			visit '/posts'
+			click_on "Create a post"
+			expect(current_path).to eq('/posts/new')
+			attach_file 'Picture', Rails.root.join('public/images/image.jpg')
+			fill_in "Caption", with: "Nice flower"
+			fill_in "Tags", with: "nature"
+			click_on "Add"
+			expect(page).to have_link("#nature")
+			expect(page).to have_content("Another random post")
+			click_on("#nature")
+			expect(page).to have_content("Posts tagged with #nature")
+			expect(page).to have_content("Nice flower")
+			expect(page).to_not have_content("Another random post")
+		end
+
+	end
+
 end
