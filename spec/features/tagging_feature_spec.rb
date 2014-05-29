a2dde57a8e5089e2124fa40c6420e0c89ffb3e1b
Sroop <sroopsunar@hotmail.com>
2014-05-29 11:35:58 +0100

    written a test to have the ability to fill in tags in the post form

--- spec/features/tagging_feature_spec.rb
+++ spec/features/tagging_feature_spec.rb
@@ -4,8 +4,20 @@ describe 'Tags' do
 
 	context 'Displaying tags on the home page' do
 
-		it 'For a post with zero tags, no tags are displayed' do
+		before do
+			user = User.create(email: 'sroop@sunar.com', password: '123456789', password_confirmation: '123456789')
+			login_as user
+		end
 
+		it 'For a post with zero tags, no tags are displayed' do
+			visit '/posts'
+			click_on "Create a post"
+			expect(current_path).to eq('/posts/new')
+			attach_file 'Picture', Rails.root.join('public/images/image.jpg')
+			fill_in "Caption", with: "Nice flower"
+			fill_in "Tags", with: ""
+			click_on "Add"
+			expect(page).to have_content("Nice flower")
 		end
 
 		it 'For a post with one tag, one tag is displayed' do
@@ -17,7 +29,7 @@ describe 'Tags' do
 		end
 
 		#unit testing in spec/models/post_spec.rb for a more detailed breakdown of the tags behaviour
-		
+
 	end
 
 end
