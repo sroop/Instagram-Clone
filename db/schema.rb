7d9abf45eac5a9223d135eba48d269a234a275db
Sroop <sroopsunar@hotmail.com>
2014-05-30 14:56:40 +0100

    hate model created and migrated. current error says it cant find a hate with the post id, need to create the relationships next

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140529123600) do
+ActiveRecord::Schema.define(version: 20140530135535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,11 @@ ActiveRecord::Schema.define(version: 20140529123600) do
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
   add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
+
+  create_table "hates", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "likes", force: true do |t|
     t.datetime "created_at"
