2084fd60cb699e03de2de56704afe8a9d8fccccd
Sroop <sroopsunar@hotmail.com>
2014-05-30 15:19:30 +0100

    hates can be created and are now rendered in the view

--- app/controllers/hates_controller.rb
+++ app/controllers/hates_controller.rb
@@ -1,6 +1,13 @@
 class HatesController < ApplicationController
 
 	def create
-		@hate = Hate.find(params[:post_id])
+		@post = Post.find(params[:post_id])
+		@hate = Hate.new
+		@post.hates << @hate
+		# @hate = Hate.new(post: @post)  -this line also adds a post id to a hate, like the above two lines
+		@hate.user = current_user
+		@hate.save
+		redirect_to '/posts'
 	end
+
 end
